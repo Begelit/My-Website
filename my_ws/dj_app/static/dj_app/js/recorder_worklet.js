e489bec8f767547cb27aff4c9b6bd13eb5c18710
Begelit <dmilevch@gmail.com>
2022-12-17 21:20:39 +0300

    128 frames

--- my_ws/dj_app/static/dj_app/js/recorder_worklet.js
+++ my_ws/dj_app/static/dj_app/js/recorder_worklet.js
@@ -6,20 +6,20 @@ class RecorderProcessor extends AudioWorkletProcessor{
         this.sample_rate = 0;
         this.buffer_length = 0;
         this.number_of_channels = 0;
-        this.visualizer_bufferLength = 0;
+        //this.visualizer_bufferLength = 0;
 
         if (options && options.processorOptions) {
             const {
                 numberOfChannels,
                 sampleRate,
                 bufferLength,
-                visualizeBufferLength,
+                //visualizeBufferLength,
             } = options.processorOptions;
     
             this.sample_rate = sampleRate;
             this.buffer_length = bufferLength;
             this.number_of_channels = numberOfChannels;
-            this.visualizer_bufferLength = visualizeBufferLength;
+            //this.visualizer_bufferLength = visualizeBufferLength;
         }
         this._recording_buffer = new Array(this.number_of_channels)
             .fill(new Float32Array(this.buffer_length));
@@ -39,33 +39,11 @@ class RecorderProcessor extends AudioWorkletProcessor{
                     this._recording_buffer[channel][this.current_bufferLength+sample] = current_sample;
                     //this.visualizer_recording_buffer[channel][this.current_visualizer_bufferLength+sample] = current_sample; 
                     outputs[input][channel][sample] = current_sample;
-                    // Sum values for visualizer
-                    //this.sampleSum += currentSample;
                 }
             }
         }
 
-        if(this.current_bufferLength + 128 < this.buffer_length){
-            this.current_bufferLength += 128;
-            /*
-            this.port.postMessage({
-                message: 'CONTINUE_BUFFER_CREATING',
-                recording_length: this.current_bufferLength,
-                buffer_array: this._recording_buffer,
-            });*/
-        } else {
-            this.port.postMessage({
-                message: 'MAX_BUFFER_LENGTH',
-                recording_length: this.current_bufferLength + 128,
-                buffer_array: this._recording_buffer,
-                vis: this.visualizer_bufferLength,
-            });
-
-            this.current_bufferLength = 0;
-            this._recording_buffer = new Array(this.number_of_channels)
-                .fill(new Float32Array(this.buffer_length));
-        }
-        /*
+       /* 
         if (this.current_visualizer_bufferLength + 128 < this.visualizer_bufferLength){
             this.current_visualizer_bufferLength += 128;
         } else {
@@ -79,13 +57,21 @@ class RecorderProcessor extends AudioWorkletProcessor{
                 .fill(new Float32Array(this.visualizer_bufferLength));
         }
         */
-        /*
-        this.port.postMessage({
-            message: 'UPDATE_VISUALIZERS',
-            inpgain: inputs,
-            outpgain: outputs,
-        });
-        */
+        if(this.current_bufferLength + 128 < this.buffer_length){
+            this.current_bufferLength += 128;
+        } else {
+            this.port.postMessage({
+                message: 'MAX_BUFFER_LENGTH',
+                recording_length: this.current_bufferLength + 128,
+                buffer_array: this._recording_buffer,
+
+            });
+
+            this.current_bufferLength = 0;
+            this._recording_buffer = new Array(this.number_of_channels)
+                .fill(new Float32Array(this.buffer_length));
+        }
+
         return true;
     }
 }
