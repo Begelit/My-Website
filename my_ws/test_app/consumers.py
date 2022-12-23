6eac0e7accd530eb8e3a08937ed63fa661498986
Begelit <dmilevch@gmail.com>
2022-12-23 05:08:29 +0300

    deploy Dmitry speech commands model

--- my_ws/test_app/consumers.py
+++ my_ws/test_app/consumers.py
@@ -13,147 +13,70 @@ from channels.generic.websocket import WebsocketConsumer
 
 
 
-
-class CNNLayerNorm(nn.Module):
-    """Layer normalization built for cnns input"""
-    def __init__(self, n_feats):
-        super(CNNLayerNorm, self).__init__()
-        self.layer_norm = nn.LayerNorm(n_feats)
-
-    def forward(self, x):
-        #Why transpose for layer norm? Find out...
-        # x (batch, channel, feature, time)
-        x = x.transpose(2, 3).contiguous() # (batch, channel, time, feature)
-        x = self.layer_norm(x)
-        return x.transpose(2, 3).contiguous() # (batch, channel, feature, time)
-class ResidualCNN(nn.Module):
-    """Residual CNN inspired by https://arxiv.org/pdf/1603.05027.pdf
-        except with layer norm instead of batch norm
-    """
-    def __init__(self, in_channels, out_channels, kernel, stride, dropout, n_feats):
-        super(ResidualCNN, self).__init__()
-        
-        self.cnn1 = nn.Conv2d(out_channels, out_channels, kernel, stride, padding=kernel//2)
-        self.cnn2 = nn.Conv2d(out_channels, out_channels, kernel, stride, padding=kernel//2)
-        self.dropout1 = nn.Dropout(dropout)
-        self.dropout2 = nn.Dropout(dropout)
-        self.layer_norm1 = CNNLayerNorm(n_feats)
-        self.layer_norm2 = CNNLayerNorm(n_feats)
-
-    def forward(self, x):
-        residual = x  # (batch, channel, feature, time)
-        x = self.layer_norm1(x)
-        x = F.gelu(x)
-        x = self.dropout1(x)
-        x = self.cnn1(x)
-        x = self.layer_norm2(x)
-        x = F.gelu(x)
-        x = self.dropout2(x)
-        x = self.cnn2(x)
-        x += residual
-        return x # (batch, channel, feature, time)
-class BidirectionalLSTM(nn.Module):
-    def __init__(self, rnn_dim, hidden_size, dropout, batch_first):
-        super(BidirectionalLSTM, self).__init__()
-        
-        self.lstm = nn.LSTM(rnn_dim, hidden_size, num_layers=1, batch_first=batch_first,
-                            bidirectional=True)
-        self.layer_norm = nn.LayerNorm(rnn_dim)
-        self.dropout = nn.Dropout(dropout)
-        
-    def forward(self, x):
-        x = self.layer_norm(x)
-        x = F.gelu(x)
-        x, _ = self.lstm(x)
-        x = self.dropout(x)
-        return x
-class SpeechRecognition(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel, stride, dropout, n_feats,
-                 rnn_dim, hidden_size, batch_first, n_classes):
-        super(SpeechRecognition, self).__init__()
-        
-        self.hcnn = nn.Conv2d(in_channels, out_channels, kernel, stride, padding=3//2)
-        
-        self.cnn = ResidualCNN(out_channels, out_channels, kernel, stride, dropout, n_feats)
-        self.fc = nn.Linear(out_channels**2, rnn_dim)
-        self.lstm = BidirectionalLSTM(rnn_dim, hidden_size, dropout, batch_first)
-        self.classifier = nn.Sequential(
-            nn.Linear(hidden_size*2, rnn_dim),
-            nn.GELU(),
-            nn.Dropout(),
-            nn.Linear(rnn_dim, n_classes)
-            )
-        
-    def forward(self, x):
-        x = self.hcnn(x)
-        x = self.cnn(x)
-        #print(x.shape)
-        sizes = x.size()
-        x = x.view(sizes[0], sizes[1] * sizes[2], sizes[3])  # (batch, feature, time)
-        x = x.transpose(1, 2)
-        #print(x.shape)
-        x = self.fc(x)
-        x = self.lstm(x)
-        x = self.classifier(x)
-        #print(x.shape)
-        return x
-
-characters_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
-            'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-            's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '<SPACE>']
-
-class TextProcessing:
-    def __init__(self, characters):
+class TextProcessed:
+    def __init__(self,characters = None,commands = None):
         if characters != None:
-            #For transcription recognization
+            #For transcription recognize
             self.characters = characters
             self.characters_map = dict()
             self.index_characters_map = dict()
             for i, character in enumerate(self.characters):
                 self.characters_map[character] = i
                 self.index_characters_map[i] = character
-        
-    def text_to_int(self, text):
-        seq = []
+        if commands != None:
+            #for classification
+            self.commands = commands
+            self.commands_dict = dict()
+            self.index_commands_dict = dict()
+            for i, command in enumerate(self.commands):
+                self.commands_dict[command] = i
+                self.index_commands_dict[i] = command
+                
+    def text2int(self, text):
+        int_list = list()
         for ch in text:
-            seq.append(self.characters_map[ch])
-        return seq
+            int_list.append(self.characters_map[ch])
+        return int_list
     
-    def int_to_text(self, seq):
-        string = []
-        for i in seq:
-            string.append(self.index_characters_map[i])
-        return ''.join(string)
+    def int2text(self, int_list):
+        ch_list = list()
+        for int_ch in int_list:
+            ch_list.append(self.index_characters_map[int_ch])
+        return ''.join(ch_list)
 
-#Initialize text processing        
+class NN2DMEL(nn.Module):
+    def __init__(self, num_class):
+        super(NN2DMEL,self).__init__()
+        
+        self.conv1 = nn.Conv2d(in_channels=1,out_channels=8,kernel_size=3,stride=1)
+        self.dropout1 = nn.Dropout(0.3) 
+    
+        self.conv2 = nn.Conv2d(in_channels=8,out_channels=16,kernel_size=3,stride=1)
+        self.dropout2 = nn.Dropout(0.3)
+        
+        self.fc1 = nn.Linear(768, 256)
+        self.dropout5 = nn.Dropout(0.3)
+        
+        self.fc2 = nn.Linear(256,128)
+        self.dropout6 = nn.Dropout(0.3)
+        
+        self.fc3 = nn.Linear(128, num_class)
 
-def GreedyDecoder(output, blank_label=26, collapse_repeated=True):
-
-    characters_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
-                'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-                's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '<SPACE>']
-
-    textprocessing = TextProcessing(characters_list)
-
-    arg_maxes = torch.argmax(output, dim=2)
-    decodes = []
-    #targets = []
-    for i, args in enumerate(arg_maxes):
-        decode = []
-        #targets.append(textprocessing.int_to_text(labels[i][:label_lengths[i]].tolist()))
-        for j, index in enumerate(args):
-            if index != blank_label:
-                if collapse_repeated and j != 0 and index == args[j -1]:
-                    continue
-                decode.append(index.item())
-        decodes.append(textprocessing.int_to_text(decode))
-    return decodes #, targets
-
-
-
-
-
-
+    def forward(self, x):
+        x = F.max_pool2d(F.relu(self.conv1(x)),kernel_size=3)
+        x = self.dropout1(x)
+        x = F.max_pool2d(F.relu(self.conv2(x)),kernel_size=3)
+        x = self.dropout2(x)
+        x = F.relu(self.fc1(x.reshape(-1,x.shape[1] * x.shape[2]*x.shape[3])))
+        x = self.dropout5(x)
+        
+        x = F.relu(self.fc2(x))
+        x = self.dropout6(x)
+        
+        x = self.fc3(x)
+        
+        #print(x.shape)
+        return x 
 
 
 
@@ -164,34 +87,46 @@ class WSConsumer(WebsocketConsumer):
 
         self.accept()
 
-        self.torch_model  = SpeechRecognition(in_channels=1,
-                          out_channels=64,
-                          kernel=3, stride=1, dropout=0.2,
-                          n_feats=64,
-                          rnn_dim=256,
-                          hidden_size=100,
-                          batch_first=True,
-                          n_classes=27
-        ) 
+        self.commands_list = ['go','stop','forward','down','left','right']
+        self.tp = TextProcessed(commands = self.commands_list)
 
-        self.optimizer = optim.Adam(self.torch_model.parameters(), lr=1e-3)
-
-        self.torch_model.load_state_dict( torch.load('third_model.pt',map_location='cpu'))
+        self.net = NN2DMEL(num_class=6)
+        self.net.load_state_dict(torch.load(
+                'epoch_194.pth',
+                map_location=torch.device('cpu')
+            )
+        )
+        self.net.eval()
 
         self.mfcc_tranform = torchaudio.transforms.MFCC(sample_rate=16000,n_mfcc=64)
+        self.mel_transform  = torchaudio.transforms.MelSpectrogram(sample_rate=16000, n_mels=64)
 
     def receive(self, text_data=None, bytes_data=None):
+
         #print("EVENT TRIGERED")
         data = json.loads(text_data)
+
         waveform = torch.tensor(list(data['array'].values()))
         print('WAVEFORM SHAPE: ', waveform.shape)
-        mfcc = self.mfcc_tranform(waveform)
-        print('MFCC SHAPE: ', mfcc.shape)
-        input_tensor =  mfcc[None,None,:,:]
+
+        #mfcc = self.mfcc_transform(waveform)
+        #print('MFCC SHAPE: ', mfcc.shape)
+        mel = self.mel_transform(waveform)
+        input_tensor =  mel[None,None,:,:]
         print('INPUT TENSOR SHAPE: ', input_tensor.shape)
-        out = self.torch_model(input_tensor)
+        out = self.net(input_tensor)
         print(out)
 
+        predicted = torch.max(out.data, 1)
+
+        decode = self.tp.index_commands_dict[int(predicted.indices)]
+        print(decode)
+        self.send(json.dumps({
+            'message': 'decode_callback',
+            'command': decode,
+            }
+        ))
+        '''
         out = F.log_softmax(out, dim=2)
         out = out.transpose(0, 1)
         print(out)
@@ -199,18 +134,5 @@ class WSConsumer(WebsocketConsumer):
         decode = GreedyDecoder(out.transpose(0, 1))
 
         print(decode)
+        '''
 
-        self.send(json.dumps({
-            'message': 'decode_callback',
-            'command': decode,
-            }
-        ))
-        #decoded_preds, decoded_targets = GreedyDecoder(out.transpose(0, 1), targets, targets_len)
-
-
-        #torchaudio.save('1.wav',waveform,16000)
-        #print(bytes_data.decode("utf-8","ignore"))
-        #self.frame_id.append(data['frame_id'])
-        #print(data['array'])
-        #arr_data_sample = np.array(list(data['array'].values()))
-        #print(max(arr_data_sample))
